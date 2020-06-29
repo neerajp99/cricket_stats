e60f55471822216994c8cb66c0949cfa4825b2a4
neerajp99 <neerajp1999@gmail.com>
2020-06-29 10:37:16 +0530

    Modifed header for all details page

--- src/components/header.js
+++ src/components/header.js
@@ -4,31 +4,36 @@ import React from "react"
 
 const Header = props => {
   const { location } = props
-  console.log(location)
   return (
     <header>
       <div className="z-10 h-10 w-full absolute">
         <Link
           to={
-            location.pathname == "/about" || location.pathname == "/all"
+            location.pathname === "/about" ||
+            location.pathname === "/all" ||
+            location.pathname === "/match"
               ? `/`
               : `/about`
           }
         >
-          <span className="z-30 float-right text-right mr-10 mt-2 font-sans font-semibold text-blue-900">
-            {location.pathname == "/about" || location.pathname == "/all"
+          <span className="cursor-pointer z-30 float-right text-right mr-10 mt-2 font-sans font-normal text-lg text-blue-900 transition duration-500 ease-in-out hover:text-blue-500">
+            {location.pathname === "/about" ||
+            location.pathname === "/all" ||
+            location.pathname === "/match"
               ? "Home"
               : "About"}
           </span>
         </Link>
         <Link
           to={
-            location.pathname == "/about" || location.pathname == "/all"
+            location.pathname === "/about" ||
+            location.pathname === "/all" ||
+            location.pathname === "/match"
               ? `/`
               : `/about`
           }
         >
-          <span className="z-10 float-right w-16 bg-blue-300 h-2 mt-6 absolute right-0 mr-8 opacity-50" />
+          <span className=" cursor-pointer z-10 float-right w-16 bg-blue-300 h-2 mt-6 absolute right-0 mr-8 opacity-50" />
         </Link>
       </div>
     </header>
