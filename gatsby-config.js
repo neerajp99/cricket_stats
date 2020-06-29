269117026812bbb1a74a8dfa12ae7de60a85d459
neerajp99 <neerajp1999@gmail.com>
2020-06-29 21:05:44 +0530

    Update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Sachin Career Stats`,
+    description: `Web Application to show Sachin Tendulkar's career stats. Made for Atlan`,
+    author: `@neerajp99`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
